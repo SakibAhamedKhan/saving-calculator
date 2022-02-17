8b72231572070d27e7af7531ff60def5c52be839
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-02-17 13:34:41 +0600

    save part done

--- js/script.js
+++ js/script.js
@@ -5,15 +5,11 @@
 function getErrorFromInput(idName){
 	const idDoc = document.getElementById(idName);
 	idDoc.style.border = '3px solid red';
-	console.log(idDoc.value);
-	console.log(idName+'-error');
-	if(isNaN(idDoc.value)){
+	const inputValue = parseFloat(idDoc.value);
+	if(isNaN(inputValue)){
 		document.getElementById(idName+'-error').innerText = '*Please Enter a Number in '+ document.getElementById(idName+'-label').innerText+' Field';
 		return true;
-	} else if(idDoc.value==''){
-		document.getElementById(idName+'-error').innerText = '*Please fillup the '+ document.getElementById(idName+'-label').innerText+' Field';
-		return true;
-	} else if(parseFloat(idDoc.value)<0){
+	} else if(inputValue<0){
 		document.getElementById(idName+'-error').innerText = '*Please Enter a Non-Negative Number in '+ document.getElementById(idName+'-label').innerText+' Field';
 		return true;
 	}
@@ -24,6 +20,7 @@ function getErrorFromInput(idName){
 	return false;
 }
 
+//Save input field if more 100
 function specialErrorSave(idName)
 {
 	const idDoc = document.getElementById(idName);
@@ -43,10 +40,16 @@ function getInputValue(idName){
 	return parseFloat(idDoc.value);
 }
 
+function getTextValue(idName){
+	const idDoc = document.getElementById(idName);
+	return parseFloat(idDoc.innerText);
+}
+
 //Set Amount in innerText
 function setAmount(idName, amount){
 	document.getElementById(idName).innerText = amount;
 }
+
 
 //Calculate Button Click Handle
 document.getElementById('calculate-button').addEventListener('click', function(){
@@ -58,7 +61,6 @@ document.getElementById('calculate-button').addEventListener('click', function()
 	{
 		let totalExpenses = getInputValue('food')+getInputValue('rent')+getInputValue('cloth');
 		let totalBalance = getInputValue('income') - totalExpenses;
-		// console.log(totalExpenses);
 		if(totalBalance<0){
 			alert('Your Income: '+getInputValue('income')+'\nYour Expenses: '+totalExpenses+'\nSo, Your Expenses is more than your Income!');
 		} else{
@@ -73,10 +75,37 @@ document.getElementById('calculate-button').addEventListener('click', function()
 
 //Save Button Click Handle
 document.getElementById('save-button').addEventListener('click', function(){
-	const error1 = getErrorFromInput('save');
-	const error2 = specialErrorSave('save');
-	if(error1==false)
-	{
+	const error1 = specialErrorSave('save');
+	
+	if(error1==false){
+		const error2 = getErrorFromInput('save');
+		if(error2==false){
+			let balance = getInputValue('income');
+			if(isNaN(balance)){	
+				alert('You Must Fill up the Income Field');
 
+			} else{
+				let savePercent = getInputValue('save');
+				let save = (balance/100)*savePercent;
+				setAmount('save-amount', save);
+				if(getTextValue('total-expenses')==0){
+					let remaining = getInputValue('income') - save;
+					setAmount('remain-balance',remaining);
+				} else{
+					let remaining = getTextValue('total-balance') - save;
+					const errorId = document.getElementById('save-error');
+					if(remaining<0){
+						errorId.innerText = '*Your balance is less than your saving';
+						document.getElementById('save').style.border = '3px solid red';
+						setAmount('remain-balance',0);
+					} else{
+						document.getElementById('save').style.border = 'none';
+						setAmount('remain-balance',remaining);
+					}
+					
+				}
+			}
+			
+		}
 	}
 });
